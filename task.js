7c7e1f037ca434cc9ebd5e97eaf95d3a2cd87335
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-07-20 08:05:01 +0300

    first-task

---
+++ task.js
@@ -0,0 +1,20 @@
+"use strict"
+const myAge = 15;
+console.log(myAge);
+
+const myName = "Artem Dykyi";
+console.log(myName);
+
+const isStudent = true;
+console.log("Чи є студентом?", isStudent);
+
+const myString = "Never give up";
+console.log(myString);
+
+let myNumber = 1;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+

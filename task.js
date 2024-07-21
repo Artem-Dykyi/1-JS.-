e7b60dde1ba8bb53209df7d2459b1b7e3fc24abc
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-07-21 12:06:54 +0300

    some-change

--- task.js
+++ task.js
@@ -18,3 +18,19 @@ console.log(myNumber);
 const myNull = null;
 console.log(myNull);
 
+const message = "Вітаємо на нашому сайті";
+// alert(message);
+// confirm("Ви згідні користуватись цим сайтом?"); 
+const clientName = prompt("Введіть ваше ім'я?");
+console.log(clientName);
+
+
+
+
+let userConfirmation = confirm("Ви впевнені, що хочете підтвердити дію?");
+
+if (userConfirmation) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}

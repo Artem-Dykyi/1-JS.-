1478cf0469e9e4103d2c64b6d09f12c7f78eb950
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-07-27 09:43:36 +0300

    second-task-half

---
+++ js/second-task.js
@@ -0,0 +1,22 @@
+// Формула конвертации Цельсия в Фаренгейты: °F = (°C × 9/5) + 32 
+let celsius = 40
+const fahrenheit = (celsius * 9/5) + 32
+console.log(fahrenheit)
+
+const dayMonth = 31
+const hourMonth = (dayMonth * 24)
+const minuteMonth = (hourMonth * 60)
+console.log(minuteMonth)
+
+let health = 100
+let energy = 100 
+const afterHealth = (health - 14)
+const afterEnergy = (energy - 47)
+console.log(afterHealth)
+console.log(afterEnergy)
+
+let price = 2400
+const sale = 0.10
+const lowPrice = (price * sale)
+console.log(lowPrice)
+

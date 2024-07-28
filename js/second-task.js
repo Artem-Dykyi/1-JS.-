b0cf6b5f9d4c5e4185457d700d995a4c3f74f8ed
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-07-28 09:57:34 +0300

    second-home

--- js/second-task.js
+++ js/second-task.js
@@ -20,3 +20,20 @@ const sale = 0.10
 const lowPrice = (price * sale)
 console.log(lowPrice)
 
+console.log(Math.floor(8.7))
+
+let someNumber = "2.2px"
+console.log(someNumber)
+// let someFullNumber = Number.parseFloat(someNumber)
+console.log(Number.parseFloat(someNumber))
+
+const anotherNumber = "44"
+console.log(Number.parseInt(anotherNumber))
+
+const numberTest = 234
+console.log(Math.sqrt(numberTest))
+
+const transformNum = "67"
+console.log(Number.parseInt(transformNum))
+// const anotherTransformNum = Number.parseInt(transformNum)
+// console.log(Event.toString(anotherTransformNum))

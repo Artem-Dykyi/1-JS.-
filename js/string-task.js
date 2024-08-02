9ec4098d24260ccdf6333eacc5e680c0b7f2ccea
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-08-02 15:46:17 +0300

    home-string-first

---
+++ js/string-task.js
@@ -0,0 +1,19 @@
+const result = 5 + 5 + "5";
+console.log(typeof result);
+
+const email = "artemdikij@53gmail.com"
+const emailLenght = email.length
+console.log(emailLenght)
+
+const my = "MY"
+const nane = "NAME"
+const is = "IS"
+const guestName = "VIKTOR"
+const fullName = my + " " + nane + " " + is;
+console.log(fullName);
+const fullUserName = fullName + " " + guestName;
+console.log(fullUserName);
+
+const userName = "Alex"
+const payment = 180 
+alert(`Дякуємо, Alex До сплати ${payment} гривень`);

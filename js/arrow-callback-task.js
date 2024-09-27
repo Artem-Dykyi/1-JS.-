b6d44b998eca7490acde179890cd863ffb2325df
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-09-27 19:11:58 +0300

    not 3-5

---
+++ js/arrow-callback-task.js
@@ -0,0 +1,63 @@
+const oneHello = () => {
+    return console.log("Hello world")
+}
+
+console.log(oneHello())    
+
+
+///////////////////////////////
+
+
+const guessNum = () => {
+    const doRandNum = Math.floor(Math.random() * 100) + 1
+    let messageRanNum = prompt("Введіть число від 0 до 100")
+    
+    return doRandNum === Number(messageRanNum)
+        ? `Число ${messageRanNum} співпало.`
+        : `Число ${messageRanNum} не співпало. Правильне число ${doRandNum}`
+    
+}
+console.log(guessNum())
+
+
+//////////////////////////////////////
+// я не розумію як це зробти (3)
+
+
+// const applyCallbackToEachEle = (arr, callback) => {
+   
+// }
+
+// const arr = [1, 2, 3, 4, 5]
+// const squareCallback = [];
+
+// const result = applyCallbackToEachEle(arr, squareCallback);
+// console.log(result)
+
+
+
+
+
+
+const calcDiscPrice = (price, discount, callback) =>{
+const resultThis = price * discountWrite
+const secondResult = price - resultThis
+console.log(secondResult)
+return resultThis
+
+}
+const discountWrite = 80 * 0.01
+
+
+
+
+const showDiscPrice = (showDiscPrice) => {
+    const messagePrice = `Знижка вийшла ${secondResult}`
+    console.log(messagePrice)
+
+    return messagePrice
+}
+
+console.log(calcDiscPrice (120, discountWrite, showDiscPrice))
+
+// не розумію що не так і чому не виводить речення (55) 

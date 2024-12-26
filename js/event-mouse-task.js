3f73ef0270653263acf02c65340c81c283ac0f3c
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2024-12-26 21:00:55 +0200

    not good

--- js/event-mouse-task.js
+++ js/event-mouse-task.js
@@ -10,3 +10,47 @@ document.addEventListener('keydown', (event) => {
 
 
 
+const controls = document.querySelector("#controls")
+const countElement = document.querySelector(".countElement")
+const createBtn = document.querySelector('[data-action="render"]')
+const deleteBtn = document.querySelector('[data-action="destroy"]')
+const createBox = document.querySelector("#boxes")
+
+
+function randomColor() {
+    const redValue = (Math.round (Math.random() * (255 - 0) + 0));
+    const greenValue = (Math.round (Math.random() * (255 - 0) + 0));
+    const blueValue = (Math.round (Math.random() * (255 - 0) + 0));
+    return `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+}
+
+
+function createBoxes(amount) {
+    const boxes = []
+    let size = 30
+
+    for(let i = 0; i < amount; i++) {
+        const box = document.createElement("div")
+        box.style.weight = `$${size}px`
+        box.style.height= `$${size}px`
+        box.style.bacgraundColor = randomColor()
+        boxes.push(box);
+        size += 10;
+    }
+    createBox.append(...boxes)
+}
+
+function destroyBoxes(){
+    createBox.innerHTML = ""
+}
+
+createBtn.addEventListener("click", () => {
+    const checkAmount = Number(countElement.value)
+    if(checkAmount === amount){
+        createBoxes()
+    }
+})
+
+deleteBtn.addEventListener("click", () => {
+    destroyBoxes()
+})
